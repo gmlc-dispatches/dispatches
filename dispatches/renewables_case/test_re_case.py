2d53e33fdd21021b1d97251b883d9d67f7af5ca7
dguittet <dguittet@nrel.gov>
2021-01-14 16:35:56 -0700

    add simple wind power unit model

---
+++ dispatches/renewables_case/test_re_case.py
@@ -0,0 +1,87 @@
+# Import objects from pyomo package
+from pyomo.environ import ConcreteModel, SolverFactory, TransformationFactory
+from pyomo.network import Arc
+
+# Import the main FlowsheetBlock from IDAES. The flowsheet block will contain the unit model
+from idaes.core import FlowsheetBlock
+
+from idaes.core.util.model_statistics import degrees_of_freedom, unfixed_variables_in_activated_equalities_set, activated_equalities_set
+
+# Import the H2 property package to create a properties block for the flowsheet
+from idaes.generic_models.properties.h2_ideal_vap import configuration as h2_config
+from idaes.generic_models.properties.core.generic.generic_property \
+    import GenericParameterBlock
+
+from idaes.generic_models.unit_models.pem_electrolyzer import PEM_Electrolyzer
+from idaes.generic_models.unit_models.wind_power import Wind_Power
+
+
+# Create the ConcreteModel and the FlowsheetBlock, and attach the flowsheet block to it.
+m = ConcreteModel()
+m.fs = FlowsheetBlock(default={"dynamic": True})   # dynamic or ss flowsheet needs to be specified here
+
+# Add properties parameter block to the flowsheet with specifications
+m.fs.properties = GenericParameterBlock(default=h2_config)
+
+print("With just the property package, the DOF is {0}".format(degrees_of_freedom(m)))
+
+m.fs.windpower = Wind_Power()
+# ((wind m/s, wind degrees from north clockwise, probability), )
+# TODO add time series resource info
+m.fs.windpower.config.resource_probability_density = ((1.5, 180, .12583),
+                                                   (10, 180, .3933),
+                                                   (15, 180, .18276),
+                                                   (20, 180, .1341),
+                                                   (24, 180, .14217),
+                                                   (30, 180, .0211))
+m.fs.windpower.system_capacity = 50000 # kW
+m.fs.windpower.initialize()
+
+print("Adding Wind, the DOF is {0}".format(degrees_of_freedom(m)))
+
+m.fs.electrolyzer = PEM_Electrolyzer(default={"property_package": m.fs.properties})
+m.fs.electrolyzer.electricity_to_mol.fix(5)
+m.fs.electrolyzer.initialize()
+
+# TODO check unit consistency
+
+m.fs.connection = Arc(source=m.fs.windpower.outlet, dest=m.fs.electrolyzer.inlet)
+TransformationFactory("network.expand_arcs").apply_to(m)
+
+
+print("Adding PEM, the DOF is {0}".format(degrees_of_freedom(m)))
+
+
+print("===Test 1===")
+solver = SolverFactory('ipopt')
+solver.solve(m.fs)
+
+print("wind eff", m.fs.windpower.efficiency.value)
+print("wind outlet kW", m.fs.windpower.outlet.electricity[0].value)
+
+print("pem inlet kW:", m.fs.electrolyzer.inlet.electricity[0].value)
+print("pem outlet H2 mols:", m.fs.electrolyzer.outlet.flow_mol[0].value)
+
+print("pem outlet temp:", m.fs.electrolyzer.outlet.temperature[0].value)
+print("pem outlet pres:", m.fs.electrolyzer.outlet.pressure[0].value)
+
+
+# Test 2 fails with exceeding state_bounds in h2_ideal_vap-- how to work with this?
+print("===Test 2===")
+m.fs.windpower.system_capacity = 50000000000
+solver.solve(m.fs)
+
+print("wind eff", m.fs.windpower.efficiency.value)
+print("wind outlet kW", m.fs.windpower.outlet.electricity[0].value)
+
+print("pem inlet kW:", m.fs.electrolyzer.inlet.electricity[0].value)
+print("pem outlet H2 mols:", m.fs.electrolyzer.outlet.flow_mol[0].value)
+
+print("pem outlet temp:", m.fs.electrolyzer.outlet.temperature[0].value)
+print("pem outlet pres:", m.fs.electrolyzer.outlet.pressure[0].value)
+
+# from six import StringIO
+# os = StringIO()
+# m.pprint(ostream=os)
+# print(os.getvalue())
+
---
+++ dispatches/renewables_case/wind_power/wind_power.py
@@ -0,0 +1,136 @@
+# Import Pyomo libraries
+from pyomo.environ import Var, Param, NonNegativeReals, units as pyunits
+from pyomo.network import Port
+from pyomo.common.config import ConfigBlock, ConfigValue, In
+
+# Import IDAES cores
+from idaes.core import (Component,
+                        ControlVolume0DBlock,
+                        declare_process_block_class,
+                        EnergyBalanceType,
+                        MomentumBalanceType,
+                        MaterialBalanceType,
+                        UnitModelBlockData,
+                        useDefault)
+from idaes.core.util.config import list_of_floats
+import idaes.logger as idaeslog
+
+import PySAM.Windpower as wind
+
+_log = idaeslog.getLogger(__name__)
+
+
+# TODO: move to idaes/core/util/config.py
+def list_of_list_of_floats(arg):
+    '''Domain validator for lists of floats
+
+    Args:
+        arg : argument to be cast to list of floats and validated
+
+    Returns:
+        List of list of floats
+    '''
+    try:
+        lst = [[float(i) for i in j] for j in arg]
+    except TypeError:
+        lst = [list_of_floats(arg), ]
+    return lst
+
+
+@declare_process_block_class("Wind_Power", doc="Wind plant using turbine powercurve and resource data")
+class WindpowerData(UnitModelBlockData):
+    """
+    Wind plant using turbine powercurve and resource data.
+    Unit model to convert wind resource into electricity.
+    """
+    CONFIG = ConfigBlock()
+    CONFIG.declare("dynamic", ConfigValue(
+        domain=In([False]),
+        default=False,
+        description="Dynamic model flag - must be False",
+        doc="""Wind plant does not support dynamic models, thus this must be False."""))
+    CONFIG.declare("has_holdup", ConfigValue(
+        default=False,
+        domain=In([False]),
+        description="Holdup construction flag",
+        doc="""Wind plant does not have defined volume, thus this must be False."""))
+    CONFIG.declare("resource_probability_density", ConfigValue(
+        default=None,
+        domain=list_of_list_of_floats,
+        description="List of (wind meters per sec, wind degrees clockwise from north, probability)",
+        doc="Probability Density Function of Wind speed [m/s] and Wind direction [degrees clockwise from N]"))
+    CONFIG.declare("resource_timeseries", ConfigValue(
+        default=None,
+        domain=list_of_list_of_floats,
+        description=""
+    ))
+
+    def build(self):
+        """Building model
+
+        Args:
+            None
+        Returns:
+            None
+        """
+        super(WindpowerData, self).build()
+
+        self.system_capacity = Param(within=NonNegativeReals,
+                                     initialize=0.0,
+                                     doc="Rated system capacity of wind farm",
+                                     units=pyunits.kW/pyunits.kW)
+
+        self.efficiency = Param(within=NonNegativeReals,
+                                initialize=0.0,
+                                doc="Ratio of power output to rated capacity",
+                                units=pyunits.kW/pyunits.kW)
+
+        self.electricity = Var(self.flowsheet().config.time,
+                               within=NonNegativeReals,
+                               initialize=0.0,
+                               doc="Electricity into control volume",
+                               units=pyunits.kW)
+
+        self.outlet = Port(noruleinit=True, doc="A port for electricity flow")
+        self.outlet.add(self.electricity, "electricity")
+
+        @self.Constraint(self.flowsheet().config.time)
+        def efficiency_curve(b, t):
+            return b.electricity[t] == self.system_capacity * self.efficiency
+
+    def _get_performance_contents(self, time_point=0):
+        return {"vars": {"Electricity": self.electricity[time_point]}}
+
+    def initialize(self, **kwargs):
+        self.setup_turbine_power()
+
+    def setup_turbine_power(self):
+        wind_simulation = wind.default("WindpowerSingleowner")
+
+        # Use ATB Turbine 2018 Market Average
+        wind_simulation.Turbine.wind_turbine_hub_ht = 88
+        wind_simulation.Turbine.wind_turbine_rotor_diameter = 116
+        wind_simulation.Turbine.wind_turbine_powercurve_windspeeds = [0.25 * i for i in range(161)]
+        wind_simulation.Turbine.wind_turbine_powercurve_powerout = [
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 55, 78, 104, 133, 167, 204, 246, 293, 345, 402, 464, 532, 606, 686,
+            772, 865, 965, 1072, 1186, 1308, 1438, 1576, 1723, 1878, 2042, 2215, 2397, 2430, 2430, 2430, 2430, 2430,
+            2430, 2430, 2430, 2430, 2430, 2430, 2430, 2430, 2430, 2430, 2430, 2430, 2430, 2430, 2430, 2430, 2430, 2430,
+            2430, 2430, 2430, 2430, 2430, 2430, 2430, 2430, 2430, 2430, 2430, 2430, 2430, 2430, 2430, 2430, 2430, 2430,
+            2430, 2430, 2430, 2430, 2430, 2430, 2430, 2430, 2430, 2430, 2430, 2430, 2430, 2430, 2430, 2430, 2430, 2430,
+            2430, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+        # Use a single turbine, do not model wake effects
+        wind_simulation.Farm.wind_farm_xCoordinates = [0]
+        wind_simulation.Farm.wind_farm_yCoordinates = [0]
+        wind_simulation.Farm.system_capacity = max(wind_simulation.Turbine.wind_turbine_powercurve_powerout)
+
+        if self.config.resource_timeseries:
+            raise NotImplementedError
+        elif self.config.resource_probability_density:
+            if abs(sum(r[2] for r in self.config.resource_probability_density) - 1) > 1e-3:
+                raise ValueError("Probabilities of Wind Speed and Direction Probability Density Function must be 1")
+            wind_simulation.Resource.wind_resource_model_choice = 2
+            wind_simulation.Resource.wind_resource_distribution = self.config.resource_probability_density
+            wind_simulation.execute(0)
+            self.efficiency = wind_simulation.Outputs.annual_energy / 8760 / wind_simulation.Farm.system_capacity
