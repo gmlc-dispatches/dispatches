80aa6cd30ff923555e433019fe87a55bac9438c8
Dan Gunter <dkgunter@lbl.gov>
2021-03-12 14:23:16 -0800

    reworked setup and README

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
+<<<<<<< Updated upstream
 # dispatches
 Primary repository for distributed dispatches software tools
+=======
+# About
+This is the primary repository for distributing dispatches software tools.
+
+DISPATCHES, the Design Integration and Synthesis Platform to Advance Tightly Coupled Hybrid Energy Systems,
+will be developed and used to identify and optimize Integrated Energy Systems for operation within the bulk 
+power system via energy market signals.
+
+DISPATCHES is part of the DOE Grid Modernization Laboratory Consortium (GMLC).
+>>>>>>> Stashed changes
--- setup.py
+++ setup.py
@@ -5,32 +5,57 @@ Project setup with setuptools
 # Always prefer setuptools over distutils
 from setuptools import setup, find_namespace_packages
 import pathlib
+import re
 
 cwd = pathlib.Path(__file__).parent.resolve()  # this will come in handy, probably
 
-long_description = """ProteusLib is an open-source, integrated suite of predictive multi-scale models
-for design and optimization of water treatment processes and systems. Specifically, ProteusLib is a new
-library of water treatment-specific property, process unit, and network models that depend on the IDAES Platform,
-an open source, next generation process systems engineering platform developed at the National Energy Technology
-Laboratory with other partners. The ProteusLib project is funded by the NAWI  as a part of U.S. Department of
-Energy’s Energy-Water Desalination Hub. The goal of ProteusLib is to assist the hub and the broader water R&D
-community in assessing existing and emerging water treatment technologies by 1) providing predictive capabilities
-involving the design, optimization, and performance of water treatment systems that will lead to improved energy
-efficiency and lower cost, 2) advancing the state of the art for the design of water treatment components, systems
-and networks to be comparable with, or even surpass, that in the chemical industry, and 3) disseminating these tools
-for active use by water treatment researchers and engineers.""".replace("\n", " ").strip()
+# Parse long description from README.md file
+with open("README.md") as f:
+    lines, capture = [], False
+    for line in f:
+        s = line.strip()
+        if re.match(r"#\s*[Aa]bout", s):
+            capture = True
+        elif re.match("^#", s):
+            break
+        elif capture is True:
+            lines.append(s)
+    if lines:
+        long_description = " ".join(lines)
+    else:
+        long_description = "DISPATCHES project"
 
-# Arguments marked as "Required" below must be included for upload to PyPI.
-# Fields marked as "Optional" may be commented out.
+
+def read_requirements(input_file):
+    """Build list of requirements from a requirements.txt file
+    """
+    req = []
+    for line in input_file:
+        s = line.strip()
+        c = s.find("#")  # look for comment
+        if c != 0:  # no comment (-1) or comment after start (> 0)
+            if c > 0:  # strip trailing comment
+                s = s[:c]
+            req.append(s)
+    return req
+
+
+with open("requirements.txt") as f:
+    package_list = read_requirements(f)
+
+with open("requirements-dev.txt") as f:
+    dev_package_list = read_requirements(f)
+
+########################################################################################
 
 setup(
-    name='dispatches',
-    url='https://github.com/gmlc-dispatches/dispatches',
-    version='0.0.1',
-    description='GMLC DISPATCHES software tools',
+    name="dispatches",
+    url="https://github.com/gmlc-dispatches/dispatches",
+    version="0.0.1",
+    description="GMLC DISPATCHES software tools",
     long_description=long_description,
-    long_description_content_type='text/plain',
-    author='DISPATCHES team',
+    long_description_content_type="text/plain",
+    author="DISPATCHES team",
     # Classifiers help users find your project by categorizing it.
     #
     # For a list of valid classifiers, see https://pypi.org/classifiers/
@@ -38,7 +63,7 @@ setup(
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
+        "Development Status :: 3 - Alpha",
         "Intended Audience :: End Users/Desktop",
         "Intended Audience :: Science/Research",
         "License :: OSI Approved :: BSD License",
@@ -55,24 +80,22 @@ setup(
         "Topic :: Scientific/Engineering :: Mathematics",
         "Topic :: Scientific/Engineering :: Chemistry",
         "Topic :: Software Development :: Libraries :: Python Modules",
-        'Programming Language :: Python :: 3 :: Only',
+        "Programming Language :: Python :: 3 :: Only",
     ],
     keywords="market simulation, chemical engineering, process modeling, hybrid power systems",
     packages=find_namespace_packages(),
+<<<<<<< Updated upstream
     python_requires='>=3.6, <4',
     install_requires=[
         'pytest',  # technically developer, but everyone likes tests
         'prescient @ git+https://github.com/grid-parity-exchange/Prescient.git'
+=======
+    python_requires=">=3.6, <4",
+    install_requires=package_list,
+    dependency_links=[
+        "git+https://github.com/grid-parity-exchange/Prescient.git#egg=prescient"
+>>>>>>> Stashed changes
     ],
-    dependency_links=['git+https://github.com/grid-parity-exchange/Prescient.git#egg=prescient'],
-    extras_require={
-         'dev': [
-             'pytest-cov'
-         ],
-    },
-   package_data={  # Optional
-        "": [
-            "*.json",
-        ],
-    },
+    extras_require={"dev": dev_package_list,},
+    package_data={"": ["*.json",],},  # Optional
 )
