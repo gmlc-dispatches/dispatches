d5c27de3db48f238e42b28e0b0c1fff711303bab
jghouse88 <>
2021-05-21 14:22:15 -0400

    price taker approach

--- dispatches/models/simple_case/stochastic_price_taker.py
+++ dispatches/models/simple_case/stochastic_price_taker.py
@@ -31,27 +31,42 @@ from idaes.core.util import get_solver
 from simple_rankine_cycle import stochastic_optimization_problem
 from matplotlib import pyplot as plt
 import numpy as np
+import pandas as pd
 from time import perf_counter
 
 # Inputs for stochastic problem
 capital_payment_years = 3
 plant_lifetime = 20
 heat_recovery = True
+p_upper_bound = 300
 
-# ARPA-E Signal
-lmp_signals = np.load("nrel_scenario_12_rep_days.npy")
+# ARPA-E Signal - NREL
+# NREL Scenario - Mid NG Price, Carbon Tax 100$, CAISO
+average_hourly = np.load("nrel_scenario_average_hourly.npy")
+rep_days = np.load("nrel_scenario_12_rep_days.npy")
+weights_rep_days = np.load("nrel_scenario_12_rep_days_weights.npy")
+raw_data = pd.read_pickle("nrel_raw_data_to_pickle.pkl")
 
-# Using single day for all year
-# price = lmp_signals[5].tolist()
+# Using average_hourly for single day for all year
+# price = average_hourly.tolist()
 # weight = 365*np.ones(len(price))
 # weight = weight.tolist()
 # power_demand = None
 
 # Using 12 representative days - equal weights
 # NREL Scenario - Mid NG Price, Carbon Tax 100$, CAISO
-price = lmp_signals.flatten().tolist()
-weight = (365/12)*np.ones(len(price))
-weight = weight.tolist()
+# price = rep_days.flatten().tolist()
+# ones_array = np.ones((len(rep_days), 24))
+# for i in range(0, len(rep_days)):
+#     ones_array[i] = weights_rep_days[i]*ones_array[i]
+# weight = ones_array.flatten().tolist()
+# power_demand = None
+
+# Using 12 representative days - equal weights
+# NREL Scenario - Mid NG Price, Carbon Tax 100$, CAISO
+price = raw_data["MiNg_$100_CAISO"].tolist()
+ones_array = np.ones(len(price))
+weight = ones_array.flatten().tolist()
 power_demand = None
 
 if __name__ == "__main__":
@@ -60,7 +75,7 @@ if __name__ == "__main__":
     m = stochastic_optimization_problem(
         heat_recovery=heat_recovery,
         capital_payment_years=capital_payment_years,
-        p_upper_bound=300,
+        p_upper_bound=p_upper_bound,
         plant_lifetime=20,
         power_demand=power_demand, lmp=price, lmp_weights=weight)
     build_toc = perf_counter()
@@ -94,6 +109,8 @@ if __name__ == "__main__":
     ax2 = ax.twinx()
     ax2.plot(p_scenario, color="blue")
     ax2.set_ylabel("Power Produced (MW)", color="blue", fontsize=14)
+    ax2.ticklabel_format(useOffset=False, style="plain")
+    ax2.set_ylim([p_min-5, optimal_p_max+5])
 
     plt.axhline(optimal_p_max, color="red", linestyle="dashed", label="p_max")
     plt.axhline(
