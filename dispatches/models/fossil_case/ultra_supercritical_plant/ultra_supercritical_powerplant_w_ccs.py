3313b45df9f5ddd044c7c9d234733cfa52292093
Naresh Susarla <susarlan@ad.science>
2021-10-06 22:08:48 -0400

    updated the connection to co2 capture system and initialization method

--- dispatches/models/fossil_case/ultra_supercritical_plant/ultra_supercritical_powerplant_w_ccs.py
+++ dispatches/models/fossil_case/ultra_supercritical_plant/ultra_supercritical_powerplant_w_ccs.py
@@ -1,42 +1,48 @@
-"""
-This is an example to study GibbsReactor module.
-"""
+##############################################################################
+# DISPATCHES was produced under the DOE Design Integration and Synthesis
+# Platform to Advance Tightly Coupled Hybrid Energy Systems program
+# (DISPATCHES), and is copyright (c) 2021 by the software owners:
+# The Regents of the University of California, through Lawrence Berkeley
+# National Laboratory, National Technology & Engineering Solutions of Sandia,
+# LLC, Alliance for Sustainable Energy, LLC, Battelle Energy Alliance, LLC,
+# University of Notre Dame du Lac, et al. All rights reserved.
+#
+# Please see the files COPYRIGHT.md and LICENSE.md for full copyright and
+# license information, respectively. Both files are also available online
+# at the URL: "https://github.com/gmlc-dispatches/dispatches".
+#
+##############################################################################
 
-# Import Pyomo libraries
-import pyomo.environ as pyo
-from pyomo.network import Arc
+"""
+This is a simple model for an ultrasupercritical coal-fired power plant
+integrated with a boiler fire-side unit model and a CO2 capture system.
+The boiler fire-side model accounts for flue-gas generation.
+The CO2 capture system uses surrogates for a solvent-based piperazine system.
+The capture rate is fixed to 90%.
+"""
 
 # IDAES Imports
-from idaes.core import FlowsheetBlock
 from idaes.core.util.model_statistics import degrees_of_freedom
-from idaes.core.util.initialization import propagate_state
 from idaes.power_generation.unit_models.boiler_fireside import BoilerFireside
-# from fluegas_ideal_vap import get_fluegas_properties
 from idaes.core.util import get_solver
 import idaes.core.util.scaling as iscale
 import idaes.logger as idaeslog
-from idaes.generic_models.unit_models import Translator
 
-# from idaes.generic_models.properties.core.generic.generic_property import (
-#     GenericParameterBlock)
 import ultra_supercritical_powerplant as usc
-from co2_capture_system import CO2Capture
 
-# from idaes.power_generation.carbon_capture.piperazine_surrogates.\
-#     co2_capture_system import CO2Capture
+from idaes.power_generation.carbon_capture.piperazine_surrogates.\
+    co2_capture_system import CO2Capture
 from idaes.power_generation.properties import FlueGasParameterBlock
 
-def add_fireside(m):
-    # fluegas_config = get_fluegas_properties(
-    #         components=['CO2', "H2O", "N2", "NO", "O2", "SO2"])
 
-    # m.fs.prop_fluegas = GenericParameterBlock(default=fluegas_config)
+def add_fireside(m):
+
     m.fs.prop_fluegas = FlueGasParameterBlock()
 
-    boiler_input_dict = {1: '614623556', # 'm.fs.boiler.heat_duty[0].value'
-                         'pl': '126983008', # 'm.fs.reheater[1].heat_duty[0].value'
-                         'roof': '96959378', # 'm.fs.reheater[2].heat_duty[0].value'
-                         'flyash': '0.0001',  # flyash or unburned carbon mass fraction
+    boiler_input_dict = {1: '614623556',
+                         'pl': '126983008',
+                         'roof': '96959378',
+                         'flyash': '0.0001',  # flyash mass fraction
                          'NOx': '140'}  # NOx PPM
 
     m.fs.boiler_fireside = BoilerFireside(
@@ -45,15 +51,15 @@ def add_fireside(m):
                      "calculate_PA_SA_flows": True,
                      "number_of_zones": 1,
                      "has_platen_superheater": True,
-                     "has_roof_superheater": True, #})
+                     "has_roof_superheater": True,
                      "surrogate_dictionary": boiler_input_dict})
     m.fs.boiler_fireside.eq_surr_waterwall_heat.deactivate()
     m.fs.boiler_fireside.eq_surr_platen_heat.deactivate()
     m.fs.boiler_fireside.eq_surr_roof_heat.deactivate()
 
     @m.fs.boiler_fireside.Constraint(m.fs.time,
-                     m.fs.boiler_fireside.zones,
-                     doc="Surrogate model for heat loss in boiler")
+                                     m.fs.boiler_fireside.zones,
+                                     doc="Waterwall heat duty")
     def eq_main_boiler_heat(b, t, z):
         return (
             b.waterwall_heat[t, z] *
@@ -61,7 +67,7 @@ def add_fireside(m):
             m.fs.boiler.heat_duty[0])
 
     @m.fs.boiler_fireside.Constraint(m.fs.time,
-                     doc="Surrogate model for heat loss in reheater 1")
+                                     doc="Reheater heat duty")
     def eq_reheater_1_heat(b, t):
         return (
             b.platen_heat[t] *
@@ -69,7 +75,7 @@ def add_fireside(m):
             m.fs.reheater[1].heat_duty[0])
 
     @m.fs.boiler_fireside.Constraint(m.fs.time,
-                     doc="Surrogate model for heat loss in reheater 2")
+                                     doc="Additional reheater heat duty")
     def eq_reheater_2_heat(b, t):
         return (
             b.roof_heat[t] *
@@ -85,7 +91,7 @@ def add_fireside(m):
     m.fs.boiler_fireside.mf_Ash_coal_dry.fix(0.109320410233292)
     m.fs.boiler_fireside.hhv_coal_dry.fix(2.581e+007)
     m.fs.boiler_fireside.frac_moisture_vaporized[:].fix(0.6)
-    m.fs.boiler_fireside.mf_H2O_coal_raw[:].fix(0.111367051)  # moisture content
+    m.fs.boiler_fireside.mf_H2O_coal_raw[:].fix(0.111367051)  # moisture
     m.fs.boiler_fireside.flowrate_coal_raw[:].fix(100.0)  # kg/s
 
     m.fs.boiler_fireside.wall_temperature_waterwall[:, :].fix(690)
@@ -124,11 +130,6 @@ def add_fireside(m):
         "pressure": 101325.00
     }
 
-    # for i in m.fs.prop_fluegas.component_list:
-    #     m.fs.boiler.primary_air_inlet.flow_mol_comp[:, i].\
-    #         fix(state_args_PA["flow_mol_comp"][i])
-    #     m.fs.boiler.secondary_air_inlet.flow_mol_comp[:, i].\
-    #         fix(state_args_SA["flow_mol_comp"][i])
     m.fs.boiler_fireside.primary_air_inlet.pressure[:].fix(101325.00)
     m.fs.boiler_fireside.secondary_air_inlet.pressure[:].fix(101325.00)
     m.fs.boiler_fireside.primary_air_inlet.temperature[:].fix(333.15)
@@ -141,33 +142,57 @@ def add_fireside(m):
     m.fs.boiler_fireside.deltaP.fix(1000)
     return m
 
+
 def add_co2capture(m):
     m.fs.co2_capture_unit = CO2Capture()
 
-    m.fs.fluegas_translator = Translator(
-        default={"outlet_state_defined": True,
-                 "inlet_property_package": m.fs.prop_fluegas,
-                 "outlet_property_package": m.fs.prop_fluegas})
+    # Adding constraints to connect flue_gas_outlet to co2_capture_unit_inlet
+    # An arc cannot be used because of the fixed components with capture unit
+    # The following are equality constraints for component flows, pressure,
+    # and temperature.
+    # TODO: update the model with a translator block to update components
+    m.fg_comp_list = ['CO2', 'H2O', 'N2', 'O2', 'NO', 'SO2']
+    m.css_comp_list = ['CO2', 'H2O', 'N2', 'O2']
 
-    m.fs.boiler_fireside.flue_gas_outlet.display()
-    m.fs.co2_capture_unit.inlet.display()
-    raise Exception()
-    m.fs.boiler_to_capture = Arc(
-        source=m.fs.boiler_fireside.flue_gas_outlet,
-        destination=m.fs.co2_capture_unit.inlet
-    )
-    pyo.TransformationFactory("network.expand_arcs").apply_to(m.fs)
+    @m.fs.co2_capture_unit.Constraint(m.fs.time,
+                                      m.css_comp_list,
+                                      doc="Flow equality constraints")
+    def eq_flow_mol_comp(b, t, c):
+        return (
+            m.fs.boiler_fireside.flue_gas_outlet.flow_mol_comp[t, c] ==
+            m.fs.co2_capture_unit.inlet.flow_mol_comp[t, c])
+
+    @m.fs.co2_capture_unit.Constraint(m.fs.time,
+                                      doc="Fixing Ar flow")
+    def eq_ar_flow_mol_comp(b, t):
+        return (
+            m.fs.co2_capture_unit.inlet.flow_mol_comp[t, 'Ar'] == 0.0089 *
+            sum(m.fs.boiler_fireside.flue_gas_outlet.flow_mol_comp[t, c]
+                for c in m.fg_comp_list))
+
+    @m.fs.co2_capture_unit.Constraint(m.fs.time,
+                                      doc="Temperature equality constraints")
+    def eq_temperature(b, t):
+        return (
+            m.fs.boiler_fireside.flue_gas_outlet.temperature[t] ==
+            m.fs.co2_capture_unit.inlet.temperature[t])
+
+    @m.fs.co2_capture_unit.Constraint(m.fs.time,
+                                      doc="Pressure equality constraints")
+    def eq_pressure(b, t):
+        return (
+            m.fs.boiler_fireside.flue_gas_outlet.pressure[t] ==
+            m.fs.co2_capture_unit.inlet.pressure[t])
+
+    m.fs.co2_capture_unit.CO2_capture_rate.fix(0.9)  # 90 % CO2 Capture
+    m.fs.co2_capture_unit.Pz_mol.fix(5)
+    m.fs.co2_capture_unit.lean_loading.fix(0.25)
 
     return m
 
-def initialize_usc_w_capture(m, fileinput=None, outlvl=idaeslog.NOTSET,
-               solver=None, optarg={}):
 
-    optarg = {
-        "max_iter": 300,
-        "halt_on_ampl_error": "yes",
-    }
-    solver = get_solver(solver, optarg)
+def initialize_usc_w_capture(m, fileinput=None, outlvl=idaeslog.NOTSET,
+                             solver=None, optarg={}):
 
     iscale.calculate_scaling_factors(m)
 
@@ -183,13 +208,38 @@ def initialize_usc_w_capture(m, fileinput=None, outlvl=idaeslog.NOTSET,
     m.fs.reheater[1].heat_duty[0].unfix()
     m.fs.reheater[2].heat_duty[0].unfix()
 
-    propagate_state(m.fs.boiler_to_capture)
-    m.fs.co2_capture_unit.initialize()
-    
+    # The initialize method in CO2Capture unit model fixes inlet state
+    # and does not unfix it. So, to use the initialize method, the
+    # constraints are deactivated before initializing and activated later.
+    # The inlet state is unfixed before activating the constraints.
+    # TODO: update this section when the initialize method in CO2Capture model
+    # is updated.
+    m.fs.co2_capture_unit.eq_flow_mol_comp.deactivate()
+    m.fs.co2_capture_unit.eq_ar_flow_mol_comp.deactivate()
+    m.fs.co2_capture_unit.eq_temperature.deactivate()
+    m.fs.co2_capture_unit.eq_pressure.deactivate()
+    m.fs.co2_capture_unit.inlet.temperature[:].fix(303.1)  # K (30 C)
+    m.fs.co2_capture_unit.inlet.pressure[:].fix(101325)  # Pa (1 atm)
+
+    m.fs.co2_capture_unit.initialize(outlvl=idaeslog.INFO)
+
+    m.fs.co2_capture_unit.inlet.temperature[:].unfix()  # K (30 C)
+    m.fs.co2_capture_unit.inlet.pressure[:].unfix()  # Pa (1 atm)
+    m.fs.co2_capture_unit.inlet.flow_mol_comp[:, 'CO2'].unfix()
+    m.fs.co2_capture_unit.inlet.flow_mol_comp[:, 'O2'].unfix()
+    m.fs.co2_capture_unit.inlet.flow_mol_comp[:, 'Ar'].unfix()
+    m.fs.co2_capture_unit.inlet.flow_mol_comp[:, 'H2O'].unfix()
+    m.fs.co2_capture_unit.inlet.flow_mol_comp[:, 'N2'].unfix()
+    m.fs.co2_capture_unit.eq_flow_mol_comp.activate()
+    m.fs.co2_capture_unit.eq_ar_flow_mol_comp.activate()
+    m.fs.co2_capture_unit.eq_temperature.activate()
+    m.fs.co2_capture_unit.eq_pressure.activate()
+
     res = solver.solve(m)
     print("Model Initialization = ",
           res.solver.termination_condition)
     print("**************  USC model w Capture Initialized   ***************")
+
 
 def build_usc_w_ccs():
 
@@ -197,10 +247,13 @@ def build_usc_w_ccs():
     solver = usc.initialize(m)
     m = add_fireside(m)
     m = add_co2capture(m)
+    assert degrees_of_freedom(m) == 0
 
-    initialize_usc_w_capture(m)
+    initialize_usc_w_capture(m, solver=solver)
+    assert degrees_of_freedom(m) == 0
 
     return m
+
 
 if __name__ == "__main__":
 
