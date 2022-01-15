2323591f750216693d0f972b5a9e2eab04a4d30a
jhjalvi <jhjalvi@sandia.gov>
2022-01-14 17:46:24 -0700

    update driver

--- dispatches/models/simple_case/rts_gmlc/prescient_run_scripts/driver_basecase.py
+++ dispatches/models/simple_case/rts_gmlc/prescient_run_scripts/driver_basecase.py
@@ -1,34 +1,49 @@
 #NOTE: This uses the `naerm` branch of Prescient on the Sandia gitlab server
 import prescient.scripts.simulator as simulator
 from prescient.scripts.runner import parse_line
+import os
 
 start_date = '01-02-2020'
 days = 364
 base_index = 0
 base_output_dir = '/home/jhjalvi/git/dispatches/dispatches/models/simple_case/rts_gmlc/prescient_results/basecase_runs/run_{}'.format(base_index)
+os.makedirs(base_output_dir, exist_ok=True)
 
-options = [
-'--data-directory=deterministic_scenarios',
-'--model-directory=/home/jhjalvi/git/prescient_idaes/prescient/models/knueven',
-'--output-directory='+base_output_dir+'deterministic_simulation_output',
-'--run-deterministic-ruc',
-'--start-date='+start_date,
-'--num-days={}'.format(days),
-'--sced-horizon=4',
-'--traceback',
-'--ruc-mipgap=0.001',
-'--deterministic-ruc-solver=gurobi_direct',
-'--deterministic-ruc-solver-options="threads=4"',
-'--sced-solver=gurobi_direct',
-'--sced-solver-options="threads=4"',
-'--ruc-horizon=36',
-'--simulator-plugin=/home/jhjalvi/git/dispatches/dispatches/models/simple_case/rts_gmlc/prescient_run_scripts/plugin_basecase_generator.py',
-'--disable-stackgraphs'
-]
+def main():
+    import prescient.scripts.simulator as simulator
+    from prescient.scripts.runner import parse_line
 
-simulator.main(args=options)
+    options = [
+    '--data-directory=deterministic_scenarios',
+    '--model-directory=/home/jhjalvi/git/prescient_idaes/prescient/models/knueven',
+    '--output-directory='+base_output_dir+'/deterministic_simulation_output_index_{}'.format(index),
+    '--run-deterministic-ruc',
+    '--start-date='+start_date,
+    '--num-days={}'.format(days),
+    '--sced-horizon=4',
+    '--traceback',
+    '--ruc-mipgap=0.001',
+    '--deterministic-ruc-solver=gurobi_direct',
+    '--deterministic-ruc-solver-options="threads=4"',
+    '--sced-solver=gurobi_direct',
+    '--sced-solver-options="threads=4"',
+    '--ruc-horizon=36',
+	'--simulator-plugin=/home/jhjalvi/git/dispatches/dispatches/models/simple_case/rts_gmlc/prescient_run_scripts/plugin_basecase_generator.py',
+    '--disable-stackgraphs'
+    ]
 
+    
+    ## redirect stdout to /dev/null
+    #import os
+    # import sys
+    # sys.stdout = open(os.devnull, 'w')
+    # print("Running Index".format(index))
+    simulator.main(args=options)
+    # sys.stdout = sys.__stdout__
+    # print("Index {} complete".format(index))
 
+if __name__ == '__main__':
+    main()
 
 
 
