85526f69ef1dad9889d83d00542c212fe68d180b
Xian <xgao1@nd.edu>
2022-03-25 13:08:19 -0400

    add script to run full year sim

--- dispatches/models/renewables_case/run_wind_battery_doubleloop.py
+++ dispatches/models/renewables_case/run_wind_battery_doubleloop.py
@@ -28,7 +28,7 @@ options = {
     },
 }
 
-Prescient().simulate(**options)
+# Prescient().simulate(**options)
 
 options = {
     "data_path": rts_gmlc_data_dir,
@@ -38,6 +38,27 @@ options = {
     "output_directory": "no_plugin_result",
     "start_date": "01-02-2020",
     "num_days": 7,
+    "sced_horizon": 4,
+    "ruc_horizon": 48,
+    "compute_market_settlements": True,
+    "day_ahead_pricing": "LMP",
+    "ruc_mipgap": 0.05,
+    "symbolic_solver_labels": True,
+    "reserve_factor": 0.0,
+    "deterministic_ruc_solver": "gurobi",
+    "sced_solver": "gurobi",
+}
+
+# Prescient().simulate(**options)
+
+options = {
+    "data_path": rts_gmlc_data_dir,
+    "input_format": "rts-gmlc",
+    "simulate_out_of_sample": True,
+    "run_sced_with_persistent_forecast_errors": True,
+    "output_directory": "no_plugin_full_year_result",
+    "start_date": "01-02-2020",
+    "num_days": 364,
     "sced_horizon": 4,
     "ruc_horizon": 48,
     "compute_market_settlements": True,
