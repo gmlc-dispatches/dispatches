86d974a096cdcdff73351a06ebc9b5e10fe6ccab
Xian <xgao1@nd.edu>
2022-04-15 09:50:16 -0400

    Add script to run sanity check

--- dispatches/models/renewables_case/run_wind_battery_doubleloop.py
+++ dispatches/models/renewables_case/run_wind_battery_doubleloop.py
@@ -35,6 +35,33 @@ options = {
     "input_format": "rts-gmlc",
     "simulate_out_of_sample": True,
     "run_sced_with_persistent_forecast_errors": True,
+    "output_directory": "test_double_loop_plugin_multiperiod_wind_battery",
+    "start_date": "01-02-2020",
+    "num_days": 3,
+    "sced_horizon": 4,
+    "ruc_horizon": 48,
+    "compute_market_settlements": True,
+    "day_ahead_pricing": "LMP",
+    "ruc_mipgap": 0.05,
+    "symbolic_solver_labels": True,
+    "reserve_factor": 0.0,
+    "deterministic_ruc_solver": "gurobi",
+    "sced_solver": "gurobi",
+    "plugin": {
+        "doubleloop": {
+            "module": "plugin_wind_battery_doubleloop.py",
+            "bidding_generator": "309_WIND_1",
+        }
+    },
+}
+
+Prescient().simulate(**options)
+
+options = {
+    "data_path": rts_gmlc_data_dir,
+    "input_format": "rts-gmlc",
+    "simulate_out_of_sample": True,
+    "run_sced_with_persistent_forecast_errors": True,
     "output_directory": "no_plugin_result",
     "start_date": "01-02-2020",
     "num_days": 7,
@@ -70,4 +97,4 @@ options = {
     "sced_solver": "gurobi",
 }
 
-Prescient().simulate(**options)
+# Prescient().simulate(**options)
