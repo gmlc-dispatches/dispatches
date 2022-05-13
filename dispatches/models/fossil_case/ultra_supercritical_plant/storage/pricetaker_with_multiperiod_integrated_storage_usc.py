7cc44b6a97db0b37c42d43cec01e49ee0a33c8e7
Naresh Susarla <susarlan@ad.science>
2022-05-12 22:51:07 -0400

    add dispatches banner and doc strings

--- dispatches/models/fossil_case/ultra_supercritical_plant/storage/pricetaker_with_multiperiod_integrated_storage_usc.py
+++ dispatches/models/fossil_case/ultra_supercritical_plant/storage/pricetaker_with_multiperiod_integrated_storage_usc.py
@@ -1,3 +1,29 @@
+##############################################################################
+# DISPATCHES was produced under the DOE Design Integration and Synthesis
+# Platform to Advance Tightly Coupled Hybrid Energy Systems program (DISPATCHES),
+# and is copyright (c) 2021 by the software owners: The Regents of the University
+# of California, through Lawrence Berkeley National Laboratory, National
+# Technology & Engineering Solutions of Sandia, LLC, Alliance for Sustainable
+# Energy, LLC, Battelle Energy Alliance, LLC, University of Notre Dame du Lac, et
+# al. All rights reserved.
+#
+# Please see the files COPYRIGHT.md and LICENSE.md for full copyright and license
+# information, respectively. Both files are also available online at the URL:
+# "https://github.com/gmlc-dispatches/dispatches".
+#
+##############################################################################
+
+"""
+This script uses the multiperiod model for the integrated ultra-supercritical
+power plant with energy storage and performs market analysis using the
+pricetaker assumption. The electricity prices, LMP (locational marginal prices)
+are assumed to not change. The prices used in this study are either obtained
+from a synthetic database.
+"""
+
+__author__ = "Naresh Susarla and Soraya Rawlings"
+
+
 from pyomo.environ import Param, Objective, Expression, SolverFactory, value
 import numpy as np
 from dispatches.models.fossil_case.ultra_supercritical_plant.storage.\
