a6f5b81540ba3d1cdffb927ff75e80d8dd17d520
Xinhe Chen <xchen24@nd.edu>
2022-06-27 10:30:01 -0400

    upload large csv file

--- dispatches/workflow/train_market_surrogates/dynamic/filter_01_6400_years.py
+++ dispatches/workflow/train_market_surrogates/dynamic/filter_01_6400_years.py
@@ -200,20 +200,20 @@ class TSA64K:
         return labels
 
 
-# def main():
+def main():
     
-#     metric = 'euclidean'
-#     years = 6400
-#     num_clusters = 50
+    metric = 'euclidean'
+    years = 6400
+    num_clusters = 50
     
-#     for i in range(10):
-#         dispatch_data = f'Dispatch_data_{i}.csv'
-#         tsa_task = TSA64K(dispatch_data, metric, years)
-#         dispatch_array = tsa_task.read_data()
-#         tsa_task.read_input_pmax()
-#         train_data,day_01 = tsa_task.transform_data(dispatch_array)
-#         labels = tsa_task.cluster_data(train_data, num_clusters, i, save_index = True)
-#         print(day_01)
+    for i in range(10):
+        dispatch_data = f'Dispatch_data_{i}.csv'
+        tsa_task = TSA64K(dispatch_data, metric, years)
+        dispatch_array = tsa_task.read_data()
+        tsa_task.read_input_pmax()
+        train_data,day_01 = tsa_task.transform_data(dispatch_array)
+        labels = tsa_task.cluster_data(train_data, num_clusters, i, save_index = True)
+        print(day_01)
 
-# if __name__ == '__main__':
-#     main()
+if __name__ == '__main__':
+    main()
