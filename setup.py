2aad3cbd8883b9f7e650d558a9ff06d816b956e7
Ludovico Bianchi <lbianchi@lbl.gov>
2022-06-30 19:39:08 -0500

    0.2.0rc0

--- setup.py
+++ setup.py
@@ -72,7 +72,7 @@ class SpecialDependencies:
     ]
 
 
-SPECIAL_DEPENDENCIES = SpecialDependencies.for_prerelease
+SPECIAL_DEPENDENCIES = SpecialDependencies.for_release
 
 
 ########################################################################################
@@ -80,7 +80,7 @@ SPECIAL_DEPENDENCIES = SpecialDependencies.for_prerelease
 setup(
     name="dispatches",
     url="https://github.com/gmlc-dispatches/dispatches",
-    version="0.2.0dev0",
+    version="0.2.0rc0",
     description="GMLC DISPATCHES software tools",
     long_description=long_description,
     long_description_content_type="text/plain",
