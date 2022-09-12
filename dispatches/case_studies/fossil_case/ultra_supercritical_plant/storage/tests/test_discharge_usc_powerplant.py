41e2c0dcdf5a55223db595fcfac9228b8749a000
Edna Soraya Rawlings <esrawli@sandia.gov>
2022-09-12 16:02:58 -0700

    Delete commented line with pytest skip

--- dispatches/case_studies/fossil_case/ultra_supercritical_plant/storage/tests/test_discharge_usc_powerplant.py
+++ dispatches/case_studies/fossil_case/ultra_supercritical_plant/storage/tests/test_discharge_usc_powerplant.py
@@ -103,7 +103,7 @@ def test_costing(model):
     discharge_usc.build_costing(model, solver=solver)
     assert degrees_of_freedom(model) == 0
 
-# @pytest.mark.skip(reason="Temporary skip after significant runtime increase with Pyomo 6.4.2")
+
 @pytest.mark.integration
 def test_usc_discharge_model(model):
     # Add missing functions to complete the discharge model (add bounds
