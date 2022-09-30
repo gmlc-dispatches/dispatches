eed6fb66c16c94ebecd581e47a47897cf1a281f7
Radhakrishna <radhakrishnatg@gmail.com>
2022-09-30 17:02:04 -0400

    pointing it to IDAES main

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ class SpecialDependencies:
         "idaes-pse==2.0.0a2",
     ]
     for_prerelease = [
-        "idaes-pse @ https://github.com/IDAES/idaes-pse/archive/2.0.0a2.zip"
+        "idaes-pse @ https://github.com/IDAES/idaes-pse/archive/main.zip"
     ]
 
 
