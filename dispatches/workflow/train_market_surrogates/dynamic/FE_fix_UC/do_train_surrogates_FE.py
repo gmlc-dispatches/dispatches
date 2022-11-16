4bc11200c2edb5a3f8f520a051396b81e7872ef8
Xinhe Chen <xchen24@nd.edu>
2022-11-16 13:50:04 -0500

    update FE case

--- dispatches/workflow/train_market_surrogates/dynamic/FE_fix_UC/do_train_surrogates_FE.py
+++ dispatches/workflow/train_market_surrogates/dynamic/FE_fix_UC/do_train_surrogates_FE.py
@@ -55,15 +55,15 @@ def main():
     # NNtrainer_rev.plot_R2_results(NN_rev_model_path, NN_rev_param_path, fig_name = f'{case_type}_revenue_plot.jpg')
 
     # # TrainNNSurrogates, dispatch frequency
-    print('Start train dispatch frequency surrogate')
-    model_type = 'frequency'
-    clustering_model_path = os.path.join(current_path,'FE_case_study','FE_result_400years_30clusters_OD_pmax_ps.json')
-    NNtrainer_df = TrainNNSurrogates(simulation_data, clustering_model_path, filter_opt = True)
-    model_df = NNtrainer_df.train_NN_frequency([input_layer_node,75,75,75,32])
-    NN_frequency_model_path = os.path.join(current_path, f'{case_type}_case_study', f'{case_type}_{num_clusters}clusters_dispatch_frequency')
-    NN_frequency_param_path = os.path.join(current_path, f'{case_type}_case_study', f'{case_type}_{num_clusters}clusters_dispatch_frequency_params.json')
-    NNtrainer_df.save_model(model_df, NN_frequency_model_path, NN_frequency_param_path)
-    NNtrainer_df.plot_R2_results(NN_frequency_model_path, NN_frequency_param_path, fig_name = f'new_{case_type}_frequency')
+    # print('Start train dispatch frequency surrogate')
+    # model_type = 'frequency'
+    # clustering_model_path = os.path.join(current_path,'FE_case_study','FE_result_400years_30clusters_OD_pmax_ps.json')
+    # NNtrainer_df = TrainNNSurrogates(simulation_data, clustering_model_path, filter_opt = True)
+    # model_df = NNtrainer_df.train_NN_frequency([input_layer_node,75,75,75,32])
+    # NN_frequency_model_path = os.path.join(current_path, f'{case_type}_case_study', f'{case_type}_{num_clusters}clusters_dispatch_frequency')
+    # NN_frequency_param_path = os.path.join(current_path, f'{case_type}_case_study', f'{case_type}_{num_clusters}clusters_dispatch_frequency_params.json')
+    # NNtrainer_df.save_model(model_df, NN_frequency_model_path, NN_frequency_param_path)
+    # NNtrainer_df.plot_R2_results(NN_frequency_model_path, NN_frequency_param_path, fig_name = f'new_{case_type}_frequency')
 
 
 
