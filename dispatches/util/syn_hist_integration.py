f6fe206d01b1a05827e40506acc6ae6e64fd7f9d
Radhakrishna <radhakrishnatg@gmail.com>
2023-03-15 21:41:10 -0400

    Run Raven within Jupyter notebook

--- dispatches/util/syn_hist_integration.py
+++ dispatches/util/syn_hist_integration.py
@@ -1,7 +1,7 @@
 #################################################################################
-# DISPATCHES was produced under the DOE Design Integration and Synthesis
-# Platform to Advance Tightly Coupled Hybrid Energy Systems program (DISPATCHES),
-# and is copyright (c) 2022 by the software owners: The Regents of the University
+# DISPATCHES was produced under the DOE Design Integration and Synthesis Platform
+# to Advance Tightly Coupled Hybrid Energy Systems program (DISPATCHES), and is
+# copyright (c) 2020-2023 by the software owners: The Regents of the University
 # of California, through Lawrence Berkeley National Laboratory, National
 # Technology & Engineering Solutions of Sandia, LLC, Alliance for Sustainable
 # Energy, LLC, Battelle Energy Alliance, LLC, University of Notre Dame du Lac, et
@@ -10,8 +10,8 @@
 # Please see the files COPYRIGHT.md and LICENSE.md for full copyright and license
 # information, respectively. Both files are also available online at the URL:
 # "https://github.com/gmlc-dispatches/dispatches".
-#
 #################################################################################
+
 __author__ = "Gabriel J. Soto, Anna Wrobel"
 
 # This file contains utility functions for generating synthetic histories from a trained reduced
