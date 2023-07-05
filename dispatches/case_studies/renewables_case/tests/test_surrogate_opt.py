7440a9c5b9d0d15bf27fc530622ff56e881747fb
dguittet <dguittet@nrel.gov>
2023-07-05 10:44:11 -0600

    fix test results

--- dispatches/case_studies/renewables_case/tests/test_surrogate_opt.py
+++ dispatches/case_studies/renewables_case/tests/test_surrogate_opt.py
@@ -26,14 +26,14 @@ def load_model():
 
 def test_RE_surrogate_steady_state_fixed():
     results = run_design(PEM_bid=30, PEM_size=200)
-    assert results['e_revenue'] == pytest.approx(-5989492, rel=1e-3)
-    assert results['h_revenue'] == pytest.approx(43656556, rel=1e-3)
-    assert results['NPV'] == pytest.approx(-1353268767, rel=1e-3)
+    assert results['e_revenue'] == pytest.approx(17901755, rel=1e-3)
+    assert results['h_revenue'] == pytest.approx(47671450, rel=1e-3)
+    assert results['NPV'] == pytest.approx(18766297, rel=1e-3)
 
 def test_RE_surrogate_steady_state():
     results = run_design()
-    assert results['pem_mw'] == pytest.approx(350.02, rel=1e-3)
-    assert results['pem_bid'] == pytest.approx(45.00, rel=1e-3)
-    assert results['e_revenue'] == pytest.approx(5777449, rel=1e-3)
-    assert results['h_revenue'] == pytest.approx(66398096, rel=1e-3)
-    assert results['NPV'] == pytest.approx(-1186120046, rel=1e-3)
+    assert results['pem_mw'] == pytest.approx(249.34, rel=1e-3)
+    assert results['pem_bid'] == pytest.approx(26.21, rel=1e-3)
+    assert results['e_revenue'] == pytest.approx(18137487, rel=1e-3)
+    assert results['h_revenue'] == pytest.approx(55074013, rel=1e-3)
+    assert results['NPV'] == pytest.approx(25546243, rel=1e-3)
