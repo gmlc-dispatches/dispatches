daf6478ec5b93785e150ebe21d79cc9aa059ff95
Adam Atia <aatia@keylogic.com>
2023-12-18 11:46:49 -0500

    update to idaes 2.2 and correspondingly, pyomo 6.6.2

--- setup.py
+++ setup.py
@@ -64,12 +64,12 @@ class SpecialDependencies:
     # idaes-pse: for IDAES DMF -dang 12/2020
     for_release = [
         # NOTE: this will fail until this idaes-pse version is available on PyPI
-        "idaes-pse==2.0.*",
-        "pyomo==6.5.*",
+        "idaes-pse==2.2.*",
+        "pyomo==6.6.2",
     ]
     for_prerelease = [
-        "idaes-pse==2.0.*",
-        "pyomo==6.5.*",
+        "idaes-pse==2.2.*",
+        "pyomo==6.6.2",
     ]
 
 
